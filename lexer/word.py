0178726c909c6507b58379dbb1faee4ed438892e
Vipul Gharde <vipul.gharde@gmail.com>
2022-05-02 05:49:29 -0400

    Reformat code

--- lexer/word.py
+++ lexer/word.py
@@ -1,21 +1,22 @@
-from lexer.token import *
 from lexer.tag import *
+from lexer.token import *
 
 
 class Word(Token, object):
-    lexeme = ""
+    lexeme = ''
 
     def __init__(self, s, tag):
         super(Word, self).__init__(tag)
         self.lexeme = s
 
-    def toString(self):
+    def __str__(self):
         return self.lexeme
 
-eq      = Word("==",         Tag.EQ)
-ne      = Word("!=",         Tag.NE)
-le      = Word("<=",         Tag.LE)
-ge      = Word(">=",         Tag.GE)
-minus   = Word("minus",   Tag.MINUS)
-TRUE    = Word("TRUE",     Tag.TRUE)
-FALSE   = Word("FALSE",   Tag.FALSE)
+
+eq = Word('==', Tag.EQ)
+ne = Word('!=', Tag.NE)
+le = Word('<=', Tag.LE)
+ge = Word('>=', Tag.GE)
+minus = Word('minus', Tag.MINUS)
+TRUE = Word('TRUE', Tag.TRUE)
+FALSE = Word('FALSE', Tag.FALSE)
